f21a05a7e06f9c45d06282b86ab82548e10a4508
marcos.achaval <marcos.achaval@bitlogic.io>
2023-01-05 12:41:04 -0300

    Project creation. First commit.

---
+++ src/main/java/guru/springcourse/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springcourse.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+
+}
